e4ab2f5337de1c198d1862935cdf43af8c8038ed
aziz jlassi <azizjlassi498@gmail.com>
2025-07-04 17:00:27 +0100

    Fixing Auth DTOs

--- src/main/java/com/aiplus/backend/auth/dto/RegisterRequest.java
+++ src/main/java/com/aiplus/backend/auth/dto/RegisterRequest.java
@@ -5,11 +5,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-
+@NoArgsConstructor
 public class RegisterRequest {
+
+    @NotBlank(message = "Username is required")
+    private String username;
 
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
@@ -18,9 +22,6 @@ public class RegisterRequest {
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
-
-    @NotBlank(message = "Username is required")
-    private String username;
 
     @NotBlank(message = "Role is required")
     private String role;
--- src/main/java/com/aiplus/backend/auth/dto/RegisterResponse.java
+++ src/main/java/com/aiplus/backend/auth/dto/RegisterResponse.java
@@ -2,9 +2,11 @@ package com.aiplus.backend.auth.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RegisterResponse {
 
     private String message;
