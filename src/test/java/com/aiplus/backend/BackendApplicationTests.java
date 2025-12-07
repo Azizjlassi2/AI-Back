add8f7582a10a8522db963c7e97b246f15faf72e
aziz jlassi <azizjlassi498@gmail.com>
2025-12-07 05:24:35 +0100

    feat(tests): Add application-test.properties and update test configuration

--- src/test/java/com/aiplus/backend/BackendApplicationTests.java
+++ src/test/java/com/aiplus/backend/BackendApplicationTests.java
@@ -2,10 +2,11 @@ package com.aiplus.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@Import(DisableMailConfig.class)
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
 	@Test
